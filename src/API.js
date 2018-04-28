4da0663ac5329833163d5d3ee053d678b393f3b7
Vernon Mensah <vernonmensah@gmail.com>
2018-04-27 20:20:29 -0400

    added .then for API calls & linter adjustment

--- src/API.js
+++ src/API.js
@@ -1,21 +1,25 @@
+/* eslint no-underscore-dangle: ["error", { "allow": ["_api"] }] */
 import axios from 'axios';
 
 class API {
-    constructor() {
-        this._api = axios.create({ baseURL: 'http://localhost:8001' });
-    }
+  constructor() {
+    this._api = axios.create({ baseURL: 'http://localhost:8001' });
+  }
 
-    getLocations() {
-        return this._api.get('locations');
-    }
+  getLocations() {
+    return this._api.get('locations')
+      .then(response => response.data);
+  }
 
-    getLocation(id) {
-        return this._api.get(`locations/${id}`);
-    }
+  getLocation(id) {
+    return this._api.get(`locations/${id}`)
+      .then(response => response);
+  }
 
-    getBuildingTypes() {
-        return this._api.get('buildingtypes');
-    }
+  getBuildingTypes() {
+    return this._api.get('buildingtypes')
+      .then(response => response);
+  }
 }
 
 export default new API();
