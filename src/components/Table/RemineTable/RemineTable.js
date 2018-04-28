d03755599dec461ec680aad731e7b051a00aedcf
Vernon Mensah <vernonmensah@gmail.com>
2018-04-27 20:21:12 -0400

    component -> func

--- src/components/Table/RemineTable/RemineTable.js
+++ src/components/Table/RemineTable/RemineTable.js
@@ -1,43 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './RemineTable.css';
 
-class RemineTable extends Component {
-    render() {    
-        return (
-            <div className="tableContainer">
-                <p>Table length: <strong>{this.props.properties.length}</strong></p>
-                <table className="remineTable">
-                    <thead>
-                        <tr>
-                            <th>Address</th>
-                            <th>Building Type</th>
-                            <th>Beds</th>
-                            <th>Baths</th>
-                        </tr>
-                    </thead>
-                    <tbody className="remineTableBody">
-                    {this.props.properties.map(property => (
-                        <tr key={property.id}>
-                            <td>{property.address}</td>
-                            <td>{property.buildingType.name}</td>
-                            <td>{property.beds}</td>
-                            <td>{property.baths}</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-            </div>
-        );
-    }
+function RemineTable(props) {
+  return (
+    <div className="tableContainer">
+      <p>Table length: <strong>{props.properties.length}</strong></p>
+      <table className="remineTable">
+        <thead>
+          <tr>
+            <th>Address</th>
+            <th>Building Type</th>
+            <th>Beds</th>
+            <th>Baths</th>
+          </tr>
+        </thead>
+        <tbody className="remineTableBody">
+          {props.properties.map(property => (
+            <tr key={property.id}>
+              <td>{property.address}</td>
+              <td>{property.buildingType.name}</td>
+              <td>{property.beds}</td>
+              <td>{property.baths}</td>
+            </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 RemineTable.defaultProps = {
-    properties: []
-}
+  properties: [],
+};
 
 RemineTable.propTypes = {
-    properties: PropTypes.array
-}
+  properties: PropTypes.arrayOf(PropTypes.object),
+};
 
 export default RemineTable;
