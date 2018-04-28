28974cb893f88909e8a59e0fc10b241bf59c2ea2
Vernon Mensah <vernonmensah@gmail.com>
2018-04-27 20:20:57 -0400

    using reg. state for err + properties

--- src/CHANGEME.js
+++ src/CHANGEME.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 import RemineTable from './components/Table/RemineTable/RemineTable';
+import APIClass from './API';
 
 class Test extends Component {
-    render() {
-        return (
-            <div className="testContainer">
-                <div className="filterContainer">
+  constructor() {
+    super();
+    this.API = APIClass;
+
+    this.state = {
+      properties: [],
+      errorMessage: null,
+    };
+  }
+  componentWillMount() {
+    // Fetching data right when component mounts
+    // & it's easier to handle errors here than in API.js
+
+    this.API.getLocations()
+      .then(data => this.setState({ properties: data }))
+      .catch(err => this.setState({ errorMessage: err }));
+  }
+
+  render() {
+    return (
+      <div className="testContainer">
+        <div className="errorMessage">{this.state.errorMessage}</div>
+        <div className="filterContainer">
                     Your filters go here.
-                </div>
-                <RemineTable properties={[]} />
-            </div>
-        );
-    }
+        </div>
+        <RemineTable properties={this.state.properties} />
+      </div>
+    );
+  }
 }
 
 export default Test;
