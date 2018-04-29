9ffa23faadfedc434e902a1ba7fbd55f4c7f0973
Vernon Mensah <vernonmensah@gmail.com>
2018-04-28 23:01:35 -0400

    template for multi filter

---
+++ src/MultiFilter.js
@@ -0,0 +1,9 @@
+/**
+ * Filters an array of items with an object of filter functions
+ * @param {array} arr Array of items.
+ * @param {object} filters An object with filter functions,
+ */
+export default function MultiFilter(arr, filters) {
+  console.log(arr, filters);
+}
+
