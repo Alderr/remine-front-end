41a74e3276fd4ee22bd8465fbf37a1d75386e20f
Vernon Mensah <vernonmensah@gmail.com>
2018-04-29 01:21:58 -0400

    space on the bottom + comp -> func

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Test from './CHANGEME.js';
+import Test from './CHANGEME';
 
 
-class App extends Component {
-    render() { 
-        return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Remine Frontend Developer Test</h2>
-                </div>
-                <p className="App-intro">
-                    Congratulations!! You have gotten farther than 75% of our applicants. Don't stop here!
-                </p>
-                <p>
-                    Do what you need to do to get this table to render filtered and fast.
-                </p>
-                <Test />
-            </div>
-        );
-    }
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Remine Frontend Developer Test</h2>
+      </div>
+      <p className="App-intro">
+        Congratulations!! You have gotten farther than 75% of our applicants. Don't stop here!
+      </p>
+      <p>
+        Do what you need to do to get this table to render filtered and fast.
+      </p>
+      <Test />
+    </div>
+  );
 }
+
 
 export default App;
