55f01c1167e6d355b63c036284cc5940755dc4b9
Vernon Mensah <vernonmensah@gmail.com>
2018-04-28 23:11:00 -0400

    v1 of multiFilter

--- src/MultiFilter.js
+++ src/MultiFilter.js
@@ -1,9 +1,10 @@
 /**
  * Filters an array of items with an object of filter functions
- * @param {array} arr Array of items.
+ * @param {array} arr Array of items. Items are objects.
  * @param {object} filters An object with filter functions,
  */
 export default function MultiFilter(arr, filters) {
-  console.log(arr, filters);
+  const filterKeys = Object.keys(filters);
+  return arr.filter(item => filterKeys.every(key => filters[key](item)));
 }
 
