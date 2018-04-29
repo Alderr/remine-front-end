e28fe48abf1b116bf319a31bcadc345aa29ba987
Vernon Mensah <vernonmensah@gmail.com>
2018-04-29 00:35:53 -0400

    v1 of index for multiple filters in the future

---
+++ src/Filters/index.js
@@ -0,0 +1,5 @@
+import BathAmountFilter from './BathAmountFilter';
+import BedAmountFilter from './BedAmountFilter';
+
+// For multiple filters
+export default { BathAmountFilter, BedAmountFilter };
